5176f30946b8b7058718f8c73dc52025088a7956
DavideTonin99 <davide9935@gmail.com>
2020-05-25 16:29:24 +0200

    definizione semaphore.h

--- semaphore.h
+++ semaphore.h
@@ -3,3 +3,18 @@
 ///         specifiche per la gestione dei SEMAFORI.
 
 #pragma once
+
+// definizione union semun
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
+
+/**
+ * semOp: funzione di support per manupolare i valori dei semafori di un set di semafori
+ * @param semid: semaphore set identifier
+ * @param sem_num: indice del semaforo nel set
+ * @param sem_op: operazione da eseguire sul semaforo semn_num
+ */
+void semOp(int semid, unsigned short sem_num, short sem_op);
