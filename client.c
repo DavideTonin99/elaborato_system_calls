07bce135217315176a41f0a8dda0b035d35b8fac
DavideTonin99 <davide9935@gmail.com>
2020-05-25 16:24:35 +0200

    format codice

--- client.c
+++ client.c
@@ -3,8 +3,10 @@
 
 #include "defines.h"
 
-int main(int argc, char * argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         printf("Usage: %s msg_queue_key\n", argv[0]);
         return 0;
     }
--- server.c
+++ server.c
@@ -7,8 +7,10 @@
 #include "semaphore.h"
 #include "fifo.h"
 
-int main(int argc, char * argv[]) {
-    if (argc < 3) {
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
         printf("Usage: %s msg_queue_key file_posizioni\n", argv[0]);
         return 0;
     }
