fd63cdedf6e9220d08478f2010224a082feb9d62
DavideTonin99 <davide9935@gmail.com>
2020-06-10 16:34:29 +0200

    added fifo functions

--- fifo.c
+++ fifo.c
@@ -4,3 +4,36 @@
 
 #include "err_exit.h"
 #include "fifo.h"
+#include "defines.h"
+
+#include "sys/stat.h"
+#include "sys/types.h"
+#include "fcntl.h"
+#include "unistd.h"
+
+void createFIFO(const char *path, mode_t mode) 
+{
+    if (mkfifo(path, mode) == -1)
+        ErrExit("mkfifo failed");
+}
+
+int openFIFO(const char *path, mode_t mode) 
+{
+    int fd = open(path, mode);
+    if (fd == -1)
+        ErrExit("open fifo failed");
+    return fd;
+}
+
+void writeFIFO(int fd, const Message *msg)
+{
+    int res = write(fd, msg, sizeof(Message));
+    if (res == -1 || res != sizeof(Message))
+        ErrExit("write message to fifo failed");
+}
+
+void closeFIFO(int fd)
+{
+    if (close(fd) == -1)
+        ErrExit("close fifo failed");
+}
--- fifo.h
+++ fifo.h
@@ -3,3 +3,25 @@
 ///         funzioni specifiche per la gestione delle FIFO.
 
 #pragma once
+#include "sys/types.h"
+#include "defines.h"
+
+/**
+ * 
+*/
+void createFIFO(const char *path, mode_t mode);
+
+/**
+ * 
+*/
+int openFIFO(const char *path, mode_t mode);
+
+/**
+ * 
+*/
+void writeFIFO(int fd, const Message *msg);
+
+/**
+ * 
+*/
+void closeFIFO(int fd);
